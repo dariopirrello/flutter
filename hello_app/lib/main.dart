788df9e5e6cf220ec11107535b4d232c3c446e63
DESKTOP-LJ2DHFE\Dario <dariopirrello.job@gmail.com>
2026-03-01 10:16:27 +0100

    First commit, hello world app

---
+++ hello_app/lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.blue,
+        ),
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Hello world!'),
+      ),
+      body: const Center(
+        child: Text(
+          '💎',
+          style: TextStyle(fontSize: 80),
+        ),
+      ),
+    );
+  }
+}
